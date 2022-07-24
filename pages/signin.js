cc6a7050a09e46a3de0f91c82f1d479774b58ba8
NHWai <nayhtetwinee@gmail.com>
2022-07-24 16:02:54 +0630

    signin page remodified

--- pages/signin.js
+++ pages/signin.js
@@ -16,8 +16,8 @@ export default function SignIn({ providers }) {
   }, [session]);
   return (
     <div className="bg-main min-h-screen grid grid-cols-1 lg:grid-cols-[2fr_1fr] ">
-      <div className="relative h-full">
-        <div className="pt-8 p-3 h-[calc(100%-40px)] flex flex-col  items-center">
+      <div className="flex flex-col">
+        <div className="pt-8 p-3 flex flex-col items-center">
           <div onClick={() => Router.back()}>
             <AiOutlineArrowLeft
               size={40}
@@ -36,32 +36,17 @@ export default function SignIn({ providers }) {
               SIGN IN
             </div>
           </div>
-          {/* <form className="p-2 max-w-sm">
-            <input
-              className="block bg-white p-2 mb-4  md:w-80 "
-              placeholder="Username or Email"
-            />
-            <input
-              className="block bg-white p-2 mb-4 md:w-80"
-              placeholder="Password"
-            />
-            <div className="text-primary text-sm font-bold">
-              Forgot your password?
-            </div>
-            <div className="block bg-primarymute cursor-pointer w-fit bg-primary text-white tracking-wider px-4 py-2 mt-8 ml-auto rounded-full">
-              SIGN IN
-            </div>
-          </form> */}
+
           <div className="mt-8 text-sm">
             Don't you have an account?{" "}
             <span className="text-primary font-bold">Create One</span>
           </div>
-          <div className="mt-8 flex items-center gap-x-8">
-            <div className="bg-black w-32 h-[1px]"></div>
+          <div className="mt-8 xl:px-16 flex items-center justify-center gap-x-8 w-full">
+            <div className="bg-slate-400 flex-auto w-32 h-[1px]"></div>
             <div>Or Sign In with</div>
-            <div className="bg-black w-32 h-[1px]"></div>
+            <div className="bg-slate-400 flex-auto w-32 h-[1px]"></div>
           </div>
-          <div className="flex mt-4 gap-x-6">
+          <div className="flex mt-8 gap-x-6">
             {Object.values(providers).map((provider) => (
               <div key={provider.name}>
                 <button
@@ -75,7 +60,7 @@ export default function SignIn({ providers }) {
             ))}
           </div>
         </div>
-        <div className="absolute bottom-0 w-full">
+        <div className="inline-block mt-auto w-full">
           <div className="w-full h-1 mt-12 bg-[#CFD4D4]"></div>
           <footer className="text-primary text-xs bg-white p-3 px-6  w-full text-center cursor-pointer">
             <div
